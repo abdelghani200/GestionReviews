3785e86dbf7093a0062ae4c77ce102ddf4aa3ece
A.Jelouani <jelouaniabdelghani@gmail.com>
2024-01-29 15:06:21 +0100

    Initial commit

---
+++ src/test/java/com/GestionReviews/GestionReviews/GestionReviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GestionReviews.GestionReviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionReviewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
