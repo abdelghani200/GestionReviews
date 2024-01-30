5d28c16216255a8ca24c2024f427f5c594cfb37e
mjsimo99 <mohashark21@gmail.com>
2024-01-30 11:32:46 +0100

    configure the Thymleaf

--- src/main/java/com/GestionReviews/GestionReviews/controller/UserController.java
+++ src/main/java/com/GestionReviews/GestionReviews/controller/UserController.java
@@ -1,4 +1,23 @@
 package com.GestionReviews.GestionReviews.controller;
 
+import com.GestionReviews.GestionReviews.model.entity.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class UserController {
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute("user") User user) {
+        return "redirect:/login";
+    }
 }
--- src/main/java/com/GestionReviews/GestionReviews/model/entity/Review.java
+++ src/main/java/com/GestionReviews/GestionReviews/model/entity/Review.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 
 @Entity
-@Table(name = "roles")
+@Table(name = "reviews")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
