8812e54e9a9d3d992434e962052425f46ff7977c
mjsimo99 <mohashark21@gmail.com>
2024-02-02 09:12:34 +0100

    security config

--- src/main/java/com/GestionReviews/GestionReviews/config/SecurityConfig.java
+++ src/main/java/com/GestionReviews/GestionReviews/config/SecurityConfig.java
@@ -2,11 +2,45 @@ package com.GestionReviews.GestionReviews.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
 @Configuration
+
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizeRequests -> {
+            authorizeRequests
+                    .requestMatchers("/css/**").permitAll()
+                    .requestMatchers("/admins/**", "/users/**").hasRole("ADMIN")
+                    .requestMatchers("/reviews/report").hasAuthority("MODERATOR")
+                    .requestMatchers("/", "/reviews/**").authenticated();
+        });
+
+        http.formLogin(formLogin -> formLogin
+                .loginPage("/login")
+                .permitAll()
+                .defaultSuccessUrl("/", true)
+                .failureUrl("/login?error=true"));
+
+        http.logout(logout -> logout
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .permitAll());
+
+        http.cors(AbstractHttpConfigurer::disable);
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.headers(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
