41ac721232bad264485e32fa75927b62cb5f7cdb
mjsimo99 <mohashark21@gmail.com>
2024-02-02 09:13:10 +0100

    review controller

--- src/main/java/com/GestionReviews/GestionReviews/controller/ReviewController.java
+++ src/main/java/com/GestionReviews/GestionReviews/controller/ReviewController.java
@@ -1,9 +1,15 @@
 package com.GestionReviews.GestionReviews.controller;
 
+import com.GestionReviews.GestionReviews.model.dto.ReviewDto;
 import com.GestionReviews.GestionReviews.service.interfaces.ReviewService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Controller
 public class ReviewController {
@@ -13,10 +19,21 @@ public class ReviewController {
         this.reviewService = reviewService;
     }
 
-    @GetMapping("/reviews")
+
+    @GetMapping("/")
     public String ViewUsersPage(Model model){
         model.addAttribute("listReviews", reviewService.getAll());
-        return "reviews";
+        model.addAttribute("reviewDto", new ReviewDto()); // Add this line
+        return "index";
+    }
+    @PostMapping("/submit-review")
+    public String submitReview(@ModelAttribute ReviewDto reviewDto) {
+        reviewDto.setUserId(1L);
+        reviewDto.setReaction("LIKE");
+        reviewDto.setDate(LocalDate.now());
+
+        reviewService.save(reviewDto);
+        return "redirect:/";
     }
 
 }
