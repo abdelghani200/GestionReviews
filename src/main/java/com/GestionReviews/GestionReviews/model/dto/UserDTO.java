7bccc9053d7822972a81eba250e6360118359bdb
mjsimo99 <mohashark21@gmail.com>
2024-02-09 15:10:33 +0100

    last push

--- src/main/java/com/GestionReviews/GestionReviews/model/dto/UserDTO.java
+++ src/main/java/com/GestionReviews/GestionReviews/model/dto/UserDTO.java
@@ -1,6 +1,5 @@
 package com.GestionReviews.GestionReviews.model.dto;
 
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
